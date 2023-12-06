a059afeffcc4b3c1bba582a38a8931f9d64dba94
Md Saayed <mdsayed.contact@gmail.com>
2023-12-06 12:25:38 +0600

    Added the redme file

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
       </QueryClientProvider>
-    </AuthProvider>
+    </AuthProvider> 
   </React.StrictMode>
 );
